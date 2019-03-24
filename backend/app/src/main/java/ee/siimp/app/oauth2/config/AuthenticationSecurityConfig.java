34d4a90428293e0bf21a2305e1b645350125a093
Siim <siim0007@hotmail.com>
2019-03-24 14:43:12 +0200

    initial oauth code

---
+++ backend/app/src/main/java/ee/siimp/app/oauth2/config/AuthenticationSecurityConfig.java
@@ -0,0 +1,39 @@
+package ee.siimp.app.oauth2.config;
+
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.annotation.Order;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+@Configuration
+@Order(1)
+public class AuthenticationSecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Autowired
+    private BCryptPasswordEncoder passwordEncoder;
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.antMatcher("/**")
+                .authorizeRequests()
+                .antMatchers("/login**")
+                .permitAll()
+                .anyRequest()
+                .authenticated()
+                .and()
+                .formLogin().permitAll();
+    }
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.inMemoryAuthentication()
+                .withUser("john")
+                .password(passwordEncoder.encode("123"))
+                .roles("USER");
+    }
+
+}
---
+++ backend/app/src/main/java/ee/siimp/app/oauth2/controller/UserController.java
@@ -0,0 +1,21 @@
+package ee.siimp.app.oauth2.controller;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.lang.invoke.MethodHandles;
+import java.security.Principal;
+
+@RestController
+public class UserController {
+
+    private static final Logger LOG = LoggerFactory.getLogger(MethodHandles.lookup().lookupClass());
+
+    @GetMapping("/user")
+    public Principal user(Principal principal) {
+        LOG.info("returning user info for user {}", principal.getName());
+        return principal;
+    }
+}
